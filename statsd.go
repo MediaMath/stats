8895692c6ffd2e173ea06853bbc9dfe2e275b447
kklipsch <junk@klipsch.net>
2016-09-13 08:17:20 -0500

    Prepare for open source (#7)
    * add contrib and license
    
    * add copyright header

--- statsd.go
+++ statsd.go
@@ -1,5 +1,9 @@
 package stats
 
+//Copyright 2016 MediaMath <http://www.mediamath.com>.  All rights reserved.
+//Use of this source code is governed by a BSD-style
+//license that can be found in the LICENSE file.
+
 import "gopkg.in/alexcesaro/statsd.v2"
 
 //StatsdEndpoint sends stats to a statsd client
